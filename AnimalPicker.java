fb80fc2a8d3d3885a261e31b85233dbaf6848d19
cakeller2 <cakeller2@quinnipiac.edu>
2024-02-26 21:36:44 -0500

    if statement for user choice

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -4,8 +4,19 @@ public class AnimalPicker {
     public static void main (String[] arg) {
         Scanner input = new Scanner(System.in); //sets up scanner
 
+        //Beginning print statments
         System.out.println("\nHello! Would you like to see the ASCII art of a dog or cat?");
         System.out.println("Please enter \"d\" for dog or \"c\" for cat");
+
+        //user picks animal
+        String animal = input.nextLine();
+
+        //if statement for user animal choice
+        if (animal.equalsIgnoreCase("d")) {
+            System.out.println("Beautiful dog ASCII art goes here");
+        } else if (animal.equalsIgnoreCase("c")){
+            System.out.println("Beautiful cat ASCII art goes here");
+        }
     } 
 }
 
